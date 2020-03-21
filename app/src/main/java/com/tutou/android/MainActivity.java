f5e1c7aa76950145cbf1ef00ed32849ce14b3f6c
Sobinbin <1298379854@qq.com>
2020-03-21 16:45:30 +0800

    first push

---
+++ app/src/main/java/com/tutou/android/MainActivity.java
@@ -0,0 +1,54 @@
+package com.tutou.android;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author Sobinbin
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+        Switch toggle = findViewById(R.id.toggle);
+        final ImageView imageView = findViewById(R.id.imageView);
+
+        // button 监听事件，将editText中字符串转发到activity_web_view中并跳转
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = editText.getText().toString();
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,WebViewActivity.class);
+                intent.putExtra("url",str);
+                startActivity(intent);
+            }
+        });
+
+        // switch 监听事件，切换imageView 图片src
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    imageView.setImageResource(R.mipmap.wuhan);
+                } else {
+                    imageView.setImageResource(R.mipmap.mmexport1550417122856);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/tutou/android/WebViewActivity.java
@@ -0,0 +1,26 @@
+package com.tutou.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * @author Sobinbin
+ */
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        WebView webView = findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        Intent intent = this.getIntent();
+        webView.loadUrl(intent.getStringExtra("url"));
+    }
+}
